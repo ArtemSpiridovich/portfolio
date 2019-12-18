33cb1ff3c8a2366b14489e2161ad067b7544359d
safronman <safronmanbox@gmail.com>
2019-12-18 16:17:09 +0300

    Updates

---
+++ precache-manifest.f8f2289a0a586f513b7d3006ef6c1eed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1393e1a7854ff5784a4ebc32634499",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f15d6b3002c07fef15ec",
+    "url": "/portfolio/static/css/main.b38482a4.chunk.css"
+  },
+  {
+    "revision": "eca80face5de6fdf87e0",
+    "url": "/portfolio/static/js/2.8beb46d2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.8beb46d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f15d6b3002c07fef15ec",
+    "url": "/portfolio/static/js/main.35871a93.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
