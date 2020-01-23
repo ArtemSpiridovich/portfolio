bf3721cc830447c297e0d389ea8ccc4a120b830c
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-01-23 19:01:39 +0300

    осталось закончить

--- src/components/ContactMe/Contacts.js
+++ src/components/ContactMe/Contacts.js
@@ -3,15 +3,35 @@ import styles from './Contacts.module.css'
 
 function Contacts() {
     return (
-        <div className={styles.contacts}>
-            <div className={styles.body}>
-                <span>Contacts</span>
-                <form className={styles.form}>
-                    <input/>
-                    <input/>
-                    <textarea></textarea>
-                </form>
-                <button>send</button>
+        <div className={styles.allElements}>
+            <div className={styles.contacts}>
+                <div className={styles.textcontact}>
+                    <div>
+                        <h2>My Contacts</h2>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur commodi ducimus eaque earum explicabo itaque, maxime mollitia qui velit voluptate. Alias animi ipsa itaque iure, molestiae odit quas soluta veritatis.</span>
+                    </div>
+                    <div>
+                        <h3>Address</h3>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cumque debitis dignissimos et iusto laboriosam quibusdam sint? Assumend...</span>
+                    </div>
+                    <div>
+                        <h3>Phone</h3>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium aliquam consequatur corporis ...</span>
+                    </div>
+                    <div>
+                        <h3>Email</h3>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+                    </div>
+                </div>
+                <div className={styles.body}>
+                    <h2>Quick Contact Form</h2>
+                    <form className={styles.form}>
+                        <div className={styles.input1}><input/><input/></div>
+                        <div className={styles.input2}><input/><input/></div>
+                        <textarea></textarea>
+                    </form>
+                    <button>send</button>
+                </div>
             </div>
         </div>
     );
