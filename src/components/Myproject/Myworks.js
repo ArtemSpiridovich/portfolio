68a95817c3fcfe12f51b8647da6c89aa83080278
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-01-25 01:22:19 +0300

    адаптивность скиллов

--- src/components/Myproject/Myworks.js
+++ src/components/Myproject/Myworks.js
@@ -4,13 +4,13 @@ import styles from './Myworks.module.css'
 function Myworks() {
     return (
         <div className={styles.myworks}>
-            <div className={styles.head}>
-                <button>watch</button>
-            </div>
-            <div className={styles.body}>
-                <h2>Name project</h2>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aut cum cupiditate debitis dicta dignissimos dolorem dolores in laboriosam maiores maxime necessitatibus, nostrum nulla numquam perspiciatis reiciendis sunt tempora voluptatibus?
-            </div>
+            {/*<div className={styles.head}>*/}
+            {/*    <button>watch</button>*/}
+            {/*</div>*/}
+            {/*<div className={styles.body}>*/}
+            {/*    <h2>Name project</h2>*/}
+            {/*    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aut cum cupiditate debitis dicta dignissimos dolorem dolores in laboriosam maiores maxime necessitatibus, nostrum nulla numquam perspiciatis reiciendis sunt tempora voluptatibus?*/}
+            {/*</div>*/}
         </div>
     );
 }
