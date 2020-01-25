d74c29e98944283ac1215783737c963d071d1c26
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-01-25 19:33:57 +0300

    поправочки

--- src/components/ContactMe/Contacts.js
+++ src/components/ContactMe/Contacts.js
@@ -10,14 +10,16 @@ function Contacts() {
                         <h2>My Contacts</h2>
                         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur commodi ducimus eaque earum explicabo itaque, maxime mollitia qui velit voluptate. Alias animi ipsa itaque iure, molestiae odit quas soluta veritatis.</span>
                     </div>
+                    <div className={styles.addressPhone}>
                     <div>
                         <h3>Address</h3>
                         <span>Belarus, Minsk, Surganova, 2</span>
                     </div>
-                    <div>
+                    <div className={styles.Phone}>
                         <h3>Phone</h3>
                         <span>+375 (33) 303-30-14</span>
                     </div>
+                </div>
                     <div>
                         <h3>Email</h3>
                         <span>artem.spirydovich@gmail.com</span>
