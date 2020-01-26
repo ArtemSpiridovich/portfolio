a8b34912903e83ec40e95e47161b72ea9de0d0a9
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-01-26 20:23:13 +0300

    footer is end

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import styles from './Footer.module.css'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
+library.add(fab)
 
 function Footer() {
     return (
@@ -7,10 +13,10 @@ function Footer() {
             <div className={styles.foot}>
                 Artem Spirydovich
                 <div className={styles.socialIcons}>
-                    <a href=''></a>
-                    <a href=''></a>
-                    <a href=''></a>
-                    <a href=''></a>
+                    <a href='https://vk.com/artitsgood'><FontAwesomeIcon icon={['fab','vk']} size='2x' /></a>
+                    <a href='https://t.me/artitsgood'><FontAwesomeIcon icon={['fab','telegram-plane']} size='2x' /></a>
+                    <a href='https://www.instagram.com/temich.art/?hl=ru'><FontAwesomeIcon icon={['fab','facebook']} size='2x' /></a>
+                    <a href='https://www.instagram.com/temich.art/?hl=ru'><FontAwesomeIcon icon={['fab','instagram']} size='2x' /></a>
                 </div>
                 <span>@2019 Все права защищены</span>
             </div>
