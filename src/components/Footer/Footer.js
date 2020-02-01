9c5b817164c8330890668a2f6a47b1cc1e82951e
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-02-01 14:54:36 +0300

    project fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from './Footer.module.css'
 import {library} from "@fortawesome/fontawesome-svg-core";
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-
-library.add(fab)
+library.add(fab);
 
 function Footer() {
     return (
@@ -13,10 +12,10 @@ function Footer() {
             <div className={styles.foot}>
                 Artem Spirydovich
                 <div className={styles.socialIcons}>
-                    <a href='https://vk.com/artitsgood'><FontAwesomeIcon icon={['fab','vk']} size='2x' /></a>
-                    <a href='https://t.me/artitsgood'><FontAwesomeIcon icon={['fab','telegram-plane']} size='2x' /></a>
-                    <a href='https://www.facebook.com/profile.php?id=100043878231093&ref=bookmarks'><FontAwesomeIcon icon={['fab','facebook']} size='2x' /></a>
-                    <a href='https://www.instagram.com/temich.art/?hl=ru'><FontAwesomeIcon icon={['fab','instagram']} size='2x' /></a>
+                    <a href='https://vk.com/artitsgood' target='_blank'><FontAwesomeIcon icon={['fab','vk']} size='2x' /></a>
+                    <a href='https://t.me/artitsgood' target='_blank'><FontAwesomeIcon icon={['fab','telegram-plane']} size='2x' /></a>
+                    <a href='https://www.facebook.com/profile.php?id=100043878231093&ref=bookmarks' target='_blank'><FontAwesomeIcon icon={['fab','facebook']} size='2x' /></a>
+                    <a href='https://www.instagram.com/temich.art/?hl=ru' target='_blank'><FontAwesomeIcon icon={['fab','instagram']} size='2x' /></a>
                 </div>
                 <span>@2019 Все права защищены</span>
             </div>
--- src/components/Myproject/Myworks.js
+++ src/components/Myproject/Myworks.js
@@ -4,13 +4,31 @@ import styles from './Myworks.module.css'
 function Myworks(props) {
     return (
         <div className={styles.myworks}>
-            {props.id===0
-            ? <div className={styles.textName1}/>
-            : props.id===1
-            ? <div className={styles.textName2}/>
-            : props.id===2
-            ? <div className={styles.textName3}/>
-            : ''
+            {props.id === 0
+                ? <div className={styles.textName1}>
+                    <a href="https://github.com/ArtemSpiridovich/game-dog" target='_blank'>
+                        <div className={styles.href1}>
+
+                        </div>
+                    </a>
+                </div>
+                : props.id === 1
+                    ? <div className={styles.textName2}>
+                        <a href="https://github.com/ArtemSpiridovich/social-network" target='_blank'>
+                            <div className={styles.href2}>
+
+                            </div>
+                        </a>
+                    </div>
+                    : props.id === 2
+                        ? <div className={styles.textName3}>
+                            <a href="https://github.com/ArtemSpiridovich/Counter" target='_blank'>
+                                <div className={styles.href3}>
+
+                                </div>
+                            </a>
+                        </div>
+                        : ''
             }
             {/*<div className={styles.head}>*/}
             {/*    <button>watch</button>*/}
