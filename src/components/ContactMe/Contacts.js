4b8a9062230e3f40ba9849ffc86b9187ee8add90
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-02-02 16:47:40 +0300

    contact end

--- src/components/ContactMe/Contacts.js
+++ src/components/ContactMe/Contacts.js
@@ -27,12 +27,16 @@ function Contacts() {
                 </div>
                 <div className={styles.body}>
                     <h2 className='test'>Quick Contact Form</h2>
-                    <form className={styles.form}>
-                        <div className={styles.input1}><input type='text' placeholder='Your Name'/><input type='email' placeholder='Your email'/></div>
-                        <div className={styles.input2}><input type='text' placeholder='Your Phone'/><input type='text' placeholder='Address'/></div>
-                        <textarea placeholder='Your message...'></textarea>
+                    <form className={styles.form}
+                          action="https://formspree.io/xknrjrzb"
+                          method="POST">
+                        <div className={styles.input1}><input name={'name'} placeholder='Your Name'/><input name={'email'} placeholder='Your email'/></div>
+                        <div className={styles.input2}><input name={'phone'} placeholder='Your Phone'/><input name={'subject'} placeholder='Subject'/></div>
+                        <textarea name={'message'} placeholder='Your message...'>
+
+                        </textarea>
+                        <button type="submit">Submit</button>
                     </form>
-                    <button>Submit</button>
                 </div>
             </div>
         </div>
